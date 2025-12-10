7551415a90c42f2e852731dc5e569b843a9a198b
Priyanka Sinha <priyanka.sinha@icts.res.in>
2025-12-10 10:52:14 +0530

    IndentationError

--- gwsurrogate_mod/new/precessing_surrogate.py
+++ gwsurrogate_mod/new/precessing_surrogate.py
@@ -1111,7 +1111,7 @@ Returns:
             hre = splinterp_many(np.array([t0]), self.t_coorb, np.real(h_inertial))
             him = splinterp_many(np.array([t0]), self.t_coorb, np.imag(h_inertial))
             h_inertial = hre + 1.j*him
-		else:
+	else:
             h_inertial= np.array([h_inertial[0]], dtype=complex)
 	
 	    
